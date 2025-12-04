ce0933fce3abfc89ad6b37365415ec450de8636f
mint <alexyoonw@gmail.com>
2025-12-03 20:27:28 -0600

    Tf-idf embedding model

--- embedding_models/tfidf_embedding_model.py
+++ embedding_models/tfidf_embedding_model.py
@@ -1,3 +1,6 @@
+import numpy as np
+import re
+from Collections import Counter, defaultdict
 from embedding_models.embedding_model import EmbeddingModel
 
 class TfIdfEmbeddingModel(EmbeddingModel):
@@ -7,14 +10,67 @@ class TfIdfEmbeddingModel(EmbeddingModel):
         """
         super().__init__()
 
+        self.vocab = {} # term -> unique index
         self.documents = []
-        self.idf_scores = {}
+        self.idf_scores = {} # term -> idf score
+        self.fitted = false # whether fit() is called
+    
+    def _tokenize(self, text: str) -> List[str]:
+        return re.findall(r"\b\w+\b", text.lower())
+
+    def add_document(self, text: str):
+        self.documents.append(text)
+
+    def fit(self):
+        """
+        Build vocabulary, compute document frequencies (DF), and compute IDF.
+        Required to calculate tf-idf embeddings
+        """
+        df = defaultdict(int)
+
+        # calculate df (each token's frequency over all documents)
+        for doc in self.documents:
+            tokens = set(self._tokenize(doc))
+            for t in tokens:
+                df[t] += 1
+
+        N = len(self.documents)
+
+        self.vocab = {term: idx for idx, term in enumerate(df.keys())}
+
+        self.idf_scores = {
+            term: math.log(N / (1 + df_val))
+            for term, df_val in df.items()
+        }
+
+        self.fitted = True
 
     def embed(self, text: str) -> np.array[float]:
-        """Generate TF-IDF embedding for the given text.
+        """
+        Generate TF-IDF embedding for the given text.
 
         Args:
             text (str): The input text to be embedded.
+
+        Returns:
+            vector of tf-idf embeddings for each term in vocabulary
         """
-        # Implementation for generating TF-IDF embeddings goes here
-        
+        if not self.fitted:
+            raise RuntimeError("Call fit() before embed().")
+
+        tokens = self._tokenize(text)
+        counter = Counter(tokens)
+
+        total = sum(counter.values())
+
+        # calculate tf (each token's frequency in the )
+        tf = {t: c / total for t, c in counter.items()}
+
+        vec = np.zeros(len(self.vocab), dtype=float)
+
+        for term, tf_val in tf.items():
+            if term in self.vocab:
+                idx = self.vocab[term]
+                vec[idx] = tf_val * self.idf_scores.get(term, 0.0)
+
+        return vec
